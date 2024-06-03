c3da1b092ce549d734c82aad06cd4593d3f6c058
stehenkesh0 <chukwuonyedika24@gmail.com>
2024-06-03 20:55:17 +0100

    Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    } 
+  },
+  plugins: [],
+}
+
